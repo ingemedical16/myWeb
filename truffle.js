b237abba31ea02525563efef3c3eb34132c53443
EL-IDRISSI_MOHAMMED <ingmedical16@gmail.com>
2019-11-21 02:56:41 +0100

    adding frontend files to Github pages

--- truffle.js
+++ truffle.js
@@ -1,5 +1,5 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-const MNEMONIC = 'apple people toss bulk index cage melt tank ranch ask bunker afford';
+require('dotenv').config();
 module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
@@ -16,11 +16,12 @@ module.exports = {
             gas: 4700000
           },
           ropsten: {
-            provider: function() {
-              return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/14ff2fabaf0c473d88d9cce458c299c0")
+            provider: function () {
+                return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY);
             },
             network_id: 3,
-            gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
-          }
+            gas: 4500000,
+            //gasPrice: 10000000000
+         }
      }
 };
