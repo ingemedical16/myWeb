d5e85cf8fee014d6cb26884c741a75023397cc4d
EL-IDRISSI_MOHAMMED <ingmedical16@gmail.com>
2019-11-26 04:09:43 +0100

    Adding frontend files to Github Pages web

--- deployfrontend.sh
+++ deployfrontend.sh
@@ -1,5 +1,5 @@
 rsync -r src/ docs/
 rsync build/contracts/PlaceDeMarche.json docs/
 git add .
-git commit -m "Adding frontend files to Github Pages"
+git commit -m "Adding frontend files to Github Pages web"
 git push
